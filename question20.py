56ebcc3e4507a91891faa9a83c2110ed9e783c9a
Eli Liu <39589318+clark-2468@users.noreply.github.com>
2023-12-04 10:02:25 +0000

    2w12e

--- question20.py
+++ question20.py
@@ -4,13 +4,55 @@ current_num=[]
 
 #generator
 #make 6 digits number using 1,1,2,2,3,3
-def make_num():
-    for i in range(1,4):
-        for j in range(1,4):
-            for k in range(1,4):
-                if i!=j and j!=k and k!=i:
-                    current_num.append(i*100+j*10+k)
-    return current_num
+valid_counter=0
+for trying_every_num in range(111111,332299):
+    current_num=str(trying_every_num)
+    #print(current_num)
+    valid=True
+    valid1=True
+    #made of 1 1 2  2 3 3
+    for counting in range (6):
+        checking=int(current_num[counting])
+        if checking >3:
+            valid1=False
+            valid=False
+            break
+        if checking==0:
+            valid1=False
+            valid=False
+            break
+    
+    #no numbers appear more than twice
+    if valid1==True:
+        for counting in range (6):
+            checking=int(current_num[counting])
+            if current_num.count(str(checking))>2:
+                valid1=False
+                valid=False
+                break
+    #no consecutive pairs of numbers
+            
+    if valid1==True:
+        first_pair=current_num[0:2]
+        second_pair=current_num[2:4]
+        third_pair=current_num[4:6]
+        fourth_pair=current_num[1:3]
+        fifth_pair=current_num[3:5]
+        if first_pair==second_pair:
+            valid=False
+        elif second_pair==third_pair:
+            valid=False
+        elif fifth_pair==fourth_pair:
+            valid=False
+        
 
-current_num=make_num()
-print(current_num)
+    if valid==True:
+        valid_counter+=1
+        print(current_num)
+
+
+
+
+
+
+print(valid_counter)
